45b5ddacd396d6813feceec07afcaf7730b53bdb
Wai Yi <wongwaverley@gmail.com>
2024-11-09 17:10:22 +0000

    arrow function examples

--- arrow-functions.js
+++ arrow-functions.js
@@ -4,11 +4,36 @@
  */
 
 // Vanilla JavaScript Function
+// function addTwoNumbers (a,b) {
+//     return a + b;
+// }
+// let sum = addTwoNumbers(1,2);
+// console.log(sum);
 
-// Arrow Function With Parameters
+// Arrow Function With Parameters (ES6)
+const addTwoNumbers = (a,b) => {
+    return a + b;
+}
+let sum = addTwoNumbers(1,2);
+console.log(addTwoNumbers(1,2));
 
 // Single Line Arrow Function With Parameters
+const addTwoNumbers2 = (a,b) => a + b;
+const addTwoNumbers3 = (a,b) => (a + b); //also valid
+console.log(addTwoNumbers2(1,2));
+console.log(addTwoNumbers3(1,2));
 
 // Implicit Returns
+const saySth = message => console.log(message);
+saySth("Hey");
+
+const sayHi = () => console.log("Hi");
+sayHi();
 
 // Returning Multiple Lines
+const multipleLine = () => (
+    `<p>
+    Hello
+    </p>`
+)
+console.log(multipleLine());
